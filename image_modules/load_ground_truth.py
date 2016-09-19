1f00aa7d8265a8ef498dbb225f001fb2c60666d4
Thomio Watanabe <thomio.watanabe@gmail.com>
2016-09-19 16:49:38 -0300

    Load Synthia ground truth

--- image_modules/load_ground_truth.py
+++ image_modules/load_ground_truth.py
@@ -27,6 +27,7 @@ def load_kitti_labels( labels_dir, object_name ):
     return label_dataset
 
 
+
 # Return bouding box pixels
 def get_bb_pixels( names, labels, nrows, ncols):
     bb = []
@@ -39,4 +40,46 @@ def get_bb_pixels( names, labels, nrows, ncols):
                 zeros[ x[0]:x[1], x[2]:x[3] ] = 1
         bb.append(zeros)
     return bb;
-    
+
+
+
+def load_synthia_gt(gt_dir, object_name, nrows, ncols):
+    # Dcitionary with synthia object classes
+    object_class =  {'void': 0,
+                     'Sky': 1,
+                     'Building': 2,
+                     'Road': 3,
+                     'Sidewalk': 4,
+                     'Fence': 5,
+                     'Vegetation': 6,
+                     'Pole': 7,
+                     'Car': 8,
+                     'Sign': 9,
+                     'Pedestrian': 10,
+                     'Cyclist': 11}
+
+    # print error if key doesn't exit:
+    if( not object_class.has_key(object_name) ):
+        print '-- Object name ', object_name,' not found in Synthia dataset.'
+        print '-- Possible options are: ', object_class.keys()
+        raise SystemExit
+
+    object_ID = object_class[object_name] #object_class.setdefault(object_name, -1)
+    print '-- Loading ' + object_name + ' ground truth... '
+
+    gt_files = os.listdir( gt_dir )
+    ground_truth_list = []
+    for gt_index, gt_name in enumerate( gt_files ):
+        gt_file = os.path.join( gt_dir, gt_name)
+        with open( gt_file ) as fileHandle:
+            array = [[int(x) for x in line.split()] for line in fileHandle]
+            ground_truth_list.append( array )
+
+    ground_truth_array = np.array( ground_truth_list )
+    # Pixels that don't belong to the object receive 0
+    for i in range( len(ground_truth_list) ):
+        non_object_index = np.where( ground_truth_array[i] != object_ID )
+        ground_truth_array[i][non_object_index] = 0
+
+    return ground_truth_list
+
