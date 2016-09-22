14a163d1f27b0b6dd24c118cbc5dd0ba9b1356ae
Thomio Watanabe <thomio.watanabe@gmail.com>
2016-09-21 21:39:47 -0300

    Rewrite class methods

--- image_modules/dataset_base.py
+++ image_modules/dataset_base.py
@@ -1,10 +1,13 @@
 from scipy import ndimage
 import numpy as np
+import abc
 import os
 
 
 # All datasets inherits from DatasetBase
 class DatasetBase:
+    __metaclass__ = abc.ABCMeta
+
     def __init__(self):
         pass
 
@@ -14,58 +17,16 @@ class DatasetBase:
         else:
             self.names, self.images = load( training_dir, self.nrows, self.ncols )
 
+    @abc.abstractmethod
+    def load_gt(self):
+        pass
+
     # divide the image in grid blocs
     def create_gt_grid( self, grid = [25,27] ):
-        print( '-- Creating grid for the ground truth...' )
-        n_bb = self.ground_truth.shape[0]
-        horizontal_size = self.ground_truth.shape[1] / grid[0]
-        vertical_size = self.ground_truth.shape[2] / grid[1]
-        self.reduced_ground_truth = np.ndarray( (n_bb , horizontal_size, vertical_size), float)
-        self.reduced_ground_truth.fill(0)
-        for k in range( n_bb ):
-            for i in range ( int(horizontal_size) ):
-                row = i * grid[0]
-                for j in range ( int(vertical_size) ):
-                    column = j * grid[1]
-                    box = self.ground_truth[k, row:(row + grid[0]), column:(column + grid[1]) ]
-                    n_ones = np.sum( box.reshape(1, box.size) == 1 )
-                    percentage = np.divide( n_ones, grid[0] * grid[1] ,dtype = float )
-                    # if more the 90% f the box have ones => the area will keep the value
-                    if ( percentage >= 0.9 ):
-                        self.reduced_ground_truth[k, i, j] = 1
-        return 0
+        self.reduced_ground_truth = reduce_gt(grid, self.ground_truth)
 
     def format_dataset( self ):
-        # Transform each 2D image in an unidimentional array
-        nimages = len( self.images )
-        num_channels = 1
-        self.images = self.images.reshape( (-1, self.nrows, self.ncols, num_channels)).astype(np.float32)
-
-        gt_nrows = self.reduced_ground_truth.shape[1]
-        gt_ncols = self.reduced_ground_truth.shape[2]
-        output_size = gt_nrows * gt_ncols
-        self.reduced_ground_truth = self.reduced_ground_truth.reshape( (-1, gt_nrows * gt_ncols)).astype(np.float32)
-
-        # Separate dataset in training, validation and test
-        # training = 60%
-        # validation and test = 20%
-        nimages_training = int( 0.6 * nimages )
-        nimages_validation = int( 0.2 * nimages )
-        nimages_test = int( 0.2 * nimages )
-
-        images_training = self.images[0:nimages_training, :]
-        # images_validation
-        # images_test
-
-        ground_truth_training = self.reduced_ground_truth[0:nimages_training, :]
-        # bb_validation
-        # bb_test
-
-        images_info = self.nrows, self.ncols, num_channels, output_size
-        data_size = nimages_training, nimages_validation, nimages_test
-        input_data = images_training, ground_truth_training
-
-        return images_info, data_size, input_data
+        return format_dataset( self.images, self.nrows, self.ncols, self.reduced_ground_truth )
 
 
 
@@ -90,6 +51,7 @@ def analyse_imgs( images ):
     # print( '-- Number of cols:', len(img[0]) )
     print( '-- Intensity max value:', np.amax(img) )
     print( '-- Intensity min value:', np.amin(img) )
+
 
 '''
 REWRITE this function to:
@@ -179,3 +141,57 @@ def load_and_analyse( training_dir, grey_scale = True ):
     analyse_imgs( images )
     images = np.array(images)
     return names, images, nrows, ncols
+
+
+def reduce_gt(grid, ground_truth):
+    print( '-- Creating grid for the ground truth...' )
+    n_bb = ground_truth.shape[0]
+    horizontal_size = ground_truth.shape[1] / grid[0]
+    vertical_size = ground_truth.shape[2] / grid[1]
+    reduced_ground_truth = np.ndarray( (n_bb , horizontal_size, vertical_size), float)
+    reduced_ground_truth.fill(0)
+    for k in range( n_bb ):
+        for i in range ( int(horizontal_size) ):
+            row = i * grid[0]
+            for j in range ( int(vertical_size) ):
+                column = j * grid[1]
+                box = ground_truth[k, row:(row + grid[0]), column:(column + grid[1]) ]
+                n_ones = np.sum( box.reshape(1, box.size) == 1 )
+                percentage = np.divide( n_ones, grid[0] * grid[1] ,dtype = float )
+                # if more the 90% f the box have ones => the area will keep the value
+                if ( percentage >= 0.9 ):
+                    reduced_ground_truth[k, i, j] = 1
+    return reduced_ground_truth
+
+
+def format_dataset( images, nrows, ncols, reduced_ground_truth ):
+    # Transform each 2D image in an unidimentional array
+    nimages = len( images )
+    num_channels = 1
+    images = images.reshape( (-1, nrows, ncols, num_channels)).astype(np.float32)
+
+    gt_nrows = reduced_ground_truth.shape[1]
+    gt_ncols = reduced_ground_truth.shape[2]
+    output_size = gt_nrows * gt_ncols
+    reduced_ground_truth = reduced_ground_truth.reshape( (-1, gt_nrows * gt_ncols)).astype(np.float32)
+
+    # Separate dataset in training, validation and test
+    # training = 60%
+    # validation and test = 20%
+    nimages_training = int( 0.6 * nimages )
+    nimages_validation = int( 0.2 * nimages )
+    nimages_test = int( 0.2 * nimages )
+
+    images_training = images[0:nimages_training, :]
+    # images_validation
+    # images_test
+
+    ground_truth_training = reduced_ground_truth[0:nimages_training, :]
+    # bb_validation
+    # bb_test
+
+    images_info = nrows, ncols, num_channels, output_size
+    data_size = nimages_training, nimages_validation, nimages_test
+    input_data = images_training, ground_truth_training
+
+    return images_info, data_size, input_data
--- image_modules/synthia_dataset.py
+++ image_modules/synthia_dataset.py
@@ -11,41 +11,45 @@ class SynthiaDataset( dataset_base.DatasetBase ):
     # Synthia dataset doesn't have a bb, each pixel has a class.
     # Thus, we don't need to load label files, we load the ground truth straight from the txt file.
     def load_gt(self, gt_dir, object_name):
-        # Dcitionary with synthia object classes
-        object_class = {'void': 0,
-                        'Sky': 1,
-                        'Building': 2,
-                        'Road': 3,
-                        'Sidewalk': 4,
-                        'Fence': 5,
-                        'Vegetation': 6,
-                        'Pole': 7,
-                        'Car': 8,
-                        'Sign': 9,
-                        'Pedestrian': 10,
-                        'Cyclist': 11}
+        self.ground_truth = load(  gt_dir, object_name )
 
-        # print error if key doesn't exit:
-        if( not object_class.has_key(object_name) ):
-            print( '-- Object name ', object_name,' not found in Synthia dataset.' )
-            print( '-- Possible options are: ', object_class.keys() )
-            raise SystemExit
 
-        object_ID = object_class[object_name]
-        print( '-- Loading ' + object_name + ' ground truth... ' )
+def load( gt_dir, object_name ):
+    # Dcitionary with synthia object classes
+    object_class = {'void': 0,
+                    'Sky': 1,
+                    'Building': 2,
+                    'Road': 3,
+                    'Sidewalk': 4,
+                    'Fence': 5,
+                    'Vegetation': 6,
+                    'Pole': 7,
+                    'Car': 8,
+                    'Sign': 9,
+                    'Pedestrian': 10,
+                    'Cyclist': 11}
 
-        gt_files = os.listdir( gt_dir )
-        ground_truth_list = []
-        for gt_index, gt_name in enumerate( gt_files ):
-            gt_file = os.path.join( gt_dir, gt_name)
-            with open( gt_file ) as fileHandle:
-                array = [[int(x) for x in line.split()] for line in fileHandle]
-                ground_truth_list.append( array )
+    # print error if key doesn't exit:
+    if object_name not in object_class:
+        print( '-- Object name ', object_name,' not found in Synthia dataset.' )
+        print( '-- Possible options are: ', object_class.keys() )
+        raise SystemExit
 
-                self.ground_truth = np.array( ground_truth_list )
-                # Pixels that don't belong to the object receive 0
-                for i in range( len(ground_truth_list) ):
-                    non_object_index = np.where( self.ground_truth[i] != object_ID )
-                    self.ground_truth[i][non_object_index] = 0
-        return 0
+    object_ID = object_class[object_name]
+    print( '-- Loading ' + object_name + ' ground truth... ' )
+
+    gt_files = os.listdir( gt_dir )
+    ground_truth_list = []
+    for gt_index, gt_name in enumerate( gt_files ):
+        gt_file = os.path.join( gt_dir, gt_name)
+        with open( gt_file ) as fileHandle:
+            array = [[int(x) for x in line.split()] for line in fileHandle]
+            ground_truth_list.append( array )
+
+            ground_truth = np.array( ground_truth_list )
+            # Pixels that don't belong to the object receive 0
+            for i in range( len(ground_truth_list) ):
+                non_object_index = np.where( ground_truth[i] != object_ID )
+                ground_truth[i][non_object_index] = 0
+    return ground_truth
 
