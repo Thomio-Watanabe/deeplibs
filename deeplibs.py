84c8a17d9633e7ddad7d78f506d289856b8b40fe
Thomio Watanabe <thomio.watanabe@gmail.com>
2016-10-07 14:24:57 -0300

    Implement train model button

--- deeplibs.py
+++ deeplibs.py
@@ -8,12 +8,12 @@ except ImportError: # python3
     from tkinter import filedialog
     from tkinter import ttk
 
+from image_modules import mnist_dataset
+from image_modules import cifar10_dataset
+from tf_modules import LeNet
+from tf_modules import AlexNet
 
-def dataset_callback(*dataset_name):
-    print( "New dataset name!" )
 
-def model_callback(*model_name):
-    print( "New model name!" )
 
 def get_img_directory():
     imgs_dir_name.set( filedialog.askdirectory() )
@@ -24,6 +24,24 @@ def get_gt_directory():
 def close_window(): 
     window.destroy()
 
+def train_model():
+    if dataset_name.get() == 'mnist':
+        dataset = mnist_dataset.MnistDataset()
+    if dataset_name.get() == 'cifar10':
+        dataset = cifar10_dataset.Cifar10Dataset()
+
+    dataset.load_images( imgs_dir_name.get() )
+    dataset.load_labels( gt_dir_name.get() )
+    dataset.format_dataset()
+
+    if model_name.get() == 'LeNet':
+        LeNet.model( dataset )
+    if model_name.get() == 'AlexNet':
+        AlexNet.model( dataset)
+
+    print('-- Finished training model.')
+
+
 
 window = tk.Tk()
 window.wm_title('DeepLibs')
@@ -33,7 +51,6 @@ dataset_label = tk.Label( window, width = 12, text="Datasets:", fg="black")
 dataset_label.grid(row=0, column=0)
 dataset_name = tk.StringVar( )
 dataset_name.set('Select dataset')
-dataset_name.trace("w", dataset_callback)
 dataset_cbox = ttk.Combobox( window, width = 15, textvariable=dataset_name, state='readonly' )
 dataset_cbox['values'] = ('mnist', 'cifar10')
 dataset_cbox.grid( row=0, column=1, padx=2, pady=2 )
@@ -42,7 +59,6 @@ model_label = tk.Label( window, width = 12, text="Models:", fg="black")
 model_label.grid( row=1, column=0 )
 model_name = tk.StringVar( )
 model_name.set('Select model')
-model_name.trace("w", model_callback)
 model_cbox = ttk.Combobox( window, width = 15, textvariable=model_name, state='readonly' )
 model_cbox['values'] = ('LeNet', 'AlexNet')
 model_cbox.grid( row=1, column=1, padx=2, pady=2 )
@@ -59,8 +75,8 @@ gt_dir_name.set('Ground truth dir')
 gt_dir_entry = tk.Entry( window, width = 16, textvariable=gt_dir_name, state='readonly' )
 gt_dir_entry.grid( row=4, column=1, padx=2, pady=2 )
 
-close_window = tk.Button(window, text ="Close", width = 12, command = close_window ).grid( row=5, column = 0 )
-run_training = tk.Button(window, text ="Train model", width = 12).grid( row=5, column = 1 )
+close = tk.Button(window, text ="Close", width = 12, command = close_window ).grid( row=5, column = 0 )
+run_training = tk.Button(window, text ="Train model", width = 12, command = train_model).grid( row=5, column = 1 )
 
 window.mainloop()
 
